ea0e2f34fe5bd17bb422a7ad8973e2fcb8eb75be
AMS <42501964+rewaaf@users.noreply.github.com>
2018-09-26 01:19:30 +0300

    Update listCounter.py

--- listCounter.py
+++ listCounter.py
@@ -2,4 +2,4 @@ lst = [[1,1],[1,1],[1,0],[1,1],[1,0,1,1],[0,1],[0,0,1]]
 counter_one = 0
 for item in lst:
     counter_one += item.count(1)
-print('number of ones in: ',str(lst),'is: ',str(counter_one))
+print('Number of ones in: ',str(lst),'is: ',str(counter_one))
